2a33b1c049b4f3e2dfd13f7962a48f9c9a00e9af
Guilherme Schumann <gui.rsc45@gmail.com>
2023-11-01 23:24:57 -0300

    add play and pause buttons

--- src/Timer.js
+++ src/Timer.js
@@ -1,6 +1,7 @@
 import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 import PlayButton from './PlayButton';
+import PauseButton from './PauseButton';
 
 const red = '#f54e4e';
 const green = '#4aec8c';
@@ -13,8 +14,12 @@ function Timer() {
                 pathColor: red,
                 trailColor: 'rgba(255,255,255,.2)',
             })}/>
+            <div style={{marginTop:'20px'}}>
+                <PlayButton/>
+                <PauseButton/>
+            </div>
             <div>
-                <PlayButton />
+                
             </div>
         </div>
     );
