51af5f8cb9eaa730866ea3907d4607fa2b363ec6
Guilherme Schumann <gui.rsc45@gmail.com>
2023-11-03 13:55:58 -0300

    create slider and now we can change settings and timer

--- src/Timer.js
+++ src/Timer.js
@@ -2,11 +2,15 @@ import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 import PlayButton from './PlayButton';
 import PauseButton from './PauseButton';
+import SettingsButton from './SettingsButton';
+import { useContext } from 'react';
+import SettingsContext from './SettingsContext';
 
 const red = '#f54e4e';
 const green = '#4aec8c';
 
 function Timer() {
+    const settingsInfo = useContext(SettingsContext);
     return (
         <div>
             <CircularProgressbar value={60} text={`${60}%`} styles={buildStyles({
@@ -18,8 +22,8 @@ function Timer() {
                 <PlayButton/>
                 <PauseButton/>
             </div>
-            <div>
-                
+            <div style={{marginTop: '20px'}}>
+                <SettingsButton onClick={() => settingsInfo.setShowSettings(true)} />
             </div>
         </div>
     );
