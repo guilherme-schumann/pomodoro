19e8c4d2a5e3042fdd6c44e19d7f28bfcbfdabd8
Guilherme Schumann <gui.rsc45@gmail.com>
2023-11-04 23:03:56 -0300

    new features

--- src/Timer.js
+++ src/Timer.js
@@ -3,7 +3,7 @@ import 'react-circular-progressbar/dist/styles.css';
 import PlayButton from './PlayButton';
 import PauseButton from './PauseButton';
 import SettingsButton from './SettingsButton';
-import { useContext } from 'react';
+import {useContext, useState, useEffect} from 'react';
 import SettingsContext from './SettingsContext';
 
 const red = '#f54e4e';
@@ -11,6 +11,20 @@ const green = '#4aec8c';
 
 function Timer() {
     const settingsInfo = useContext(SettingsContext);
+
+    const [isPaused, setIsPaused] = useState(false);
+    const [mode, setMode] = useState('work'); // work/break/null
+    const [secoundsLeft, setSecoundsLeft] = useState(0);
+
+    function initTimer() {
+        setSecoundsLeft:(settingsInfo.workMinutes * 60);
+    }
+
+    useEffect(() => {initTimer()}, settingsInfo);
+
+    setInterval(() => {1000}, settingsInfo);
+
+
     return (
         <div>
             <CircularProgressbar value={60} text={`${60}%`} styles={buildStyles({
@@ -19,8 +33,7 @@ function Timer() {
                 trailColor: 'rgba(255,255,255,.2)',
             })}/>
             <div style={{marginTop:'20px'}}>
-                <PlayButton/>
-                <PauseButton/>
+                {isPaused ? <PlayButton /> : <PauseButton />}
             </div>
             <div style={{marginTop: '20px'}}>
                 <SettingsButton onClick={() => settingsInfo.setShowSettings(true)} />
